ebd608b30eb98d46a2beed57c2d04debdf5feb4e
Kostya <relond4186@gmail.com>
2022-10-04 15:35:52 +0300

    Перевод введеных данный в массив

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите каждый элемент массива через пробел");
-int[]element=GetArraystring(Console.ReadLine());
+string[]element=GetArraystring(Console.ReadLine()); 
 
 
-
-int[]GetArraystring(string stringArray)
+string[]GetArraystring(string stringArray)
 {
     string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    int[] result = new int[numS.Length];
-    for (int i = 0; i < result.Length; i++)
-    {
-        result[i]= int.Parse(numS[i]);
-    }
-    return result;
+    return numS;
 }
+
 
