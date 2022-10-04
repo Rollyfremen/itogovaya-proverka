2bd3c1844524e4fce7cec4d60430ec517745c445
Kostya <relond4186@gmail.com>
2022-10-04 15:00:09 +0300

    Начало программы

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите каждый элемент массива через пробел");
+int[]element=GetArraystring(Console.ReadLine());
+
+
+
+int[]GetArraystring(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i]= int.Parse(numS[i]);
+    }
+    return result;
+}
+
