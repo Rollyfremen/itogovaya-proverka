a58aee1f39ba8a07a28efbeb7ff5886bc1e7268e
Kostya <relond4186@gmail.com>
2022-10-04 15:44:25 +0300

    Законченая программа

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите каждый элемент массива через пробел");
 string[]element=GetArraystring(Console.ReadLine()); 
-
+for (int i = 0; i <= element.Length ; i++)
+{
+      if (element[i].Length <= 3)
+      {
+            Console.WriteLine($" {element[i]}");
+      }
+}
 
 string[]GetArraystring(string stringArray)
 {
